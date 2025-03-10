752aa1a77009375ce0de29a3755f7b1954823dba
tauhid-taaha <127302904+tauhid-taaha@users.noreply.github.com>
2025-03-10 13:29:39 +0600

    server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
+import cors from 'cors'
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+app.use((req, res, next) => {
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+    next();
+  });
+  
+app.get('/', (req,res)=>{
+    res.send("<h1>Welcome</h1>");
+})
+;
+
+
+const PORT=process.env.PORT ;
+
+app.listen(PORT,()=>{
+    console.log(`server running on ${PORT}`.bgCyan.white);
+});
